03ddf979fe64cc56c12301517ddb75ceb385c02d
Emil Risberg <45082589+ERisberg@users.noreply.github.com>
2024-11-09 16:17:06 +0100

    Create constants.ts

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const GENERIC_DROPS_CONTAINER_ID = "#drops";
+export const STREAMER_DROPS_CONTAINER_ID = ".streamer-drops";
+export const DROP_ID = ".drop-box";
+export const DROP_LOCK_ID = ".drop-lock";
